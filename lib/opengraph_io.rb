f098f53a85fb58a80c10af8d15fec943219a3260
William Dana <william.b.dana@gmail.com>
2018-06-30 11:41:46 -0400

    Add OpenGraphIO#get_site_info_url and #get_site_info_query_params methods

--- lib/opengraph_io.rb
+++ lib/opengraph_io.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class OpenGraphIO
+  require 'cgi'
   require 'net/http'
   attr_accessor :app_id, :cache_ok, :full_render, :version
+
 
   def initialize(options)
     # Throw an error if app_id is not present in options hash
@@ -14,6 +16,7 @@ class OpenGraphIO
     @app_id = options[:app_id]
 
     # Assign options if present, or defaults if not
+    # These can be overridden when making requests through OpenGraphIO#get_site_info
     @cache_ok =
       options.has_key?(:cache_ok) ?
         options[:cache_ok] :
@@ -27,4 +30,26 @@ class OpenGraphIO
         options[:version] :
         '1.1'
   end
+
+  def get_site_info_url(url)
+    'https://opengraph.io/api' + @version + '/site/' + CGI.escape(url)
+  end
+
+  def get_site_info_query_params(options)
+    query_string_values = {}
+
+    query_string_values[:cache_ok] =
+      options.has_key?(:cache_ok) ?
+        options[:cache_ok] :
+        @cache_ok
+
+    query_string_values[:app_id] = 
+      options.has_key?(:app_id) ?
+        options[:app_id] :
+        @app_id
+    
+    query_string_values
+  end
+
+  
 end
